e9d2d97c6120b7395e58ef3895d3598699703d37
Hollis Lau <hollislau@hotmail.com>
2015-05-25 21:59:07 -0700

    Add transitions to donut shop to improve user interactivity

--- shop.js
+++ shop.js
@@ -51,12 +51,18 @@
   }
 
   topPot.prototype.renderOption = function(index) {
-    var option = document.createElement("option");
+    var updateOption = document.createElement("option");
+    var removeOption = document.createElement("option");
 
-    option.textContent = this.name;
-    option.value = index;
+    updateOption.textContent = this.name;
+    updateOption.value = index;
+    updateOption.setAttribute("class", "update");
+    removeOption.textContent = this.name;
+    removeOption.value = index;
+    removeOption.setAttribute("class", "remove");
 
-    document.getElementById("select").appendChild(option);
+    document.getElementById("update-dropdown").appendChild(updateOption);
+    document.getElementById("remove-dropdown").appendChild(removeOption);
   }
 
   var shops = [];
@@ -64,7 +70,7 @@
   shops.push(new topPot("Downtown", 8, 43, 4.5));
   shops.push(new topPot("Capitol Hill", 4, 37, 2));
   shops.push(new topPot("S. Lake Union", 9, 23, 6.33));
-  shops.push(new topPot("Wedgewood", 2, 28, 1.25));
+  shops.push(new topPot("Wedgwood", 2, 28, 1.25));
   shops.push(new topPot("Ballard", 8, 58, 3.75));
 
   var generateCalc = function() {
@@ -88,92 +94,121 @@
   }
 
   var addNewShop = function() {
-    var locationName = document.getElementById("location");
-    var minCust = document.getElementById("min-cust");
-    var maxCust = document.getElementById("max-cust");
-    var avgDonuts = document.getElementById("avg-donuts-cust");
-    var newShop = new topPot(locationName.value, parseFloat(minCust.value), parseFloat(maxCust.value), parseFloat(avgDonuts.value))
+    var locationAdd = document.getElementById("location-add");
+    var minCustAdd = document.getElementById("min-cust-add");
+    var maxCustAdd = document.getElementById("max-cust-add");
+    var avgDonutsAdd = document.getElementById("avg-donuts-cust-add");
 
-    if (locationName.value && minCust.value && maxCust.value && avgDonuts.value) {
-      newShop.generateRandom();
-      newShop.donutsHourCalc();
-      newShop.donutsDayCalc();
+    if (locationAdd.value && minCustAdd.value && maxCustAdd.value && avgDonutsAdd.value) {
+      var addShop = new topPot(locationAdd.value, parseFloat(minCustAdd.value), parseFloat(maxCustAdd.value), parseFloat(avgDonutsAdd.value));
+      addShop.generateRandom();
+      addShop.donutsHourCalc();
+      addShop.donutsDayCalc();
 
-      shops.push(newShop);
+      shops.push(addShop);
 
-      newShop.renderRow();
-      newShop.renderOption(shops.length - 1);
+      addShop.renderRow();
+      addShop.renderOption(shops.length - 1);
 
-      locationName.value = "";
-      minCust.value = "";
-      maxCust.value = "";
-      avgDonuts.value = "";
+      // locationAdd.value = "";
+      // minCustAdd.value = "";
+      // maxCustAdd.value = "";
+      // avgDonutsAdd.value = "";
     }
 
     console.log(shops);
   }
 
   var populateUpdate = function() {
-    var selectPopulate = document.getElementById("select");
+    var populateDropdown = document.getElementById("update-dropdown");
+    var locationPopulate = document.getElementById("location-update");
     var minCustPopulate = document.getElementById("min-cust-update");
     var maxCustPopulate = document.getElementById("max-cust-update");
     var avgDonutsPopulate = document.getElementById("avg-donuts-cust-update");
-    var selectShop = shops[selectPopulate.value];
+    var shopToPopulate = shops[populateDropdown.value];
 
-    minCustPopulate.value = selectShop.minCustHour;
-    maxCustPopulate.value = selectShop.maxCustHour;
-    avgDonutsPopulate.value = selectShop.avgDonutsCust;
+    locationPopulate.value = shopToPopulate.name;
+    minCustPopulate.value = shopToPopulate.minCustHour;
+    maxCustPopulate.value = shopToPopulate.maxCustHour;
+    avgDonutsPopulate.value = shopToPopulate.avgDonutsCust;
 
-    console.log(selectPopulate.value);
+    console.log(populateDropdown.value);
   }
 
   var updateShop = function() {
-    var locationUpdate = document.getElementById("select");
+    var updateDropdown = document.getElementById("update-dropdown");
+    var locationUpdate = document.getElementById("location-update");
     var minCustUpdate = document.getElementById("min-cust-update");
     var maxCustUpdate = document.getElementById("max-cust-update");
     var avgDonutsUpdate = document.getElementById("avg-donuts-cust-update");
-    var updateShop = shops[locationUpdate.value];
+    var updateOptionList = document.getElementsByClassName("update");
+    var removeOptionList = document.getElementsByClassName("remove");
+    var shopToUpdate = shops[updateDropdown.value];
 
-    if (minCustUpdate.value) {
-      updateShop.minCustHour = parseFloat(minCustUpdate.value);
-      if (maxCustUpdate.value) {
-        updateShop.maxCustHour = parseFloat(maxCustUpdate.value);
-        if (avgDonutsUpdate.value) {
-          updateShop.avgDonutsCust = parseFloat(avgDonutsUpdate.value);
-        }
-      } else if (avgDonutsUpdate.value) {
-        updateShop.avgDonutsCust = parseFloat(avgDonutsUpdate.value);
-      }
-    } else if (maxCustUpdate.value) {
-      updateShop.maxCustHour = parseFloat(maxCustUpdate.value);
-      if (avgDonutsUpdate.value) {
-        updateShop.avgDonutsCust = parseFloat(avgDonutsUpdate.value);
-      }
-    } else if (avgDonutsUpdate.value) {
-      updateShop.avgDonutsCust = parseFloat(avgDonutsUpdate.value);
+    if (locationUpdate.value && minCustUpdate.value && maxCustUpdate.value && avgDonutsUpdate.value) {
+      shopToUpdate.name = locationUpdate.value;
+      shopToUpdate.minCustHour = parseFloat(minCustUpdate.value);
+      shopToUpdate.maxCustHour = parseFloat(maxCustUpdate.value);
+      shopToUpdate.avgDonutsCust = parseFloat(avgDonutsUpdate.value);
+
+      shopToUpdate.generateRandom();
+      shopToUpdate.donutsHourCalc();
+      shopToUpdate.donutsDayCalc();
+
+      document.getElementById("donut-estimate").innerHTML = "";
+      renderTable();
+
+      updateOptionList[updateDropdown.value].textContent = locationUpdate.value;
+      removeOptionList[updateDropdown.value].textContent = locationUpdate.value;
+
+      // updateDropdown.value = "";
+      // locationUpdate.value = "";
+      // minCustUpdate.value = "";
+      // maxCustUpdate.value = "";
+      // avgDonutsUpdate.value = "";
     }
-
-    updateShop.generateRandom();
-    updateShop.donutsHourCalc();
-    updateShop.donutsDayCalc();
-
-    document.getElementById("donut-estimate").innerHTML = "";
-    renderTable();
-
-    minCustUpdate.value = "";
-    maxCustUpdate.value = "";
-    avgDonutsUpdate.value = "";
 
     console.log(shops);
   }
 
-  var submitButton = document.getElementById("submit");
-  var updateButton = document.getElementById("update");
-  var selectButton = document.getElementById("select");
+  var removeShop = function() {
+    var updateDropdown = document.getElementById("update-dropdown");
+    var removeDropdown = document.getElementById("remove-dropdown");
+    var updateOptionList = document.getElementsByClassName("update");
+    var removeOptionList = document.getElementsByClassName("remove");
 
-  submitButton.addEventListener("click", addNewShop, false);
+    if (removeDropdown.value) {
+      if (confirm("Permanently remove the " + shops[removeDropdown.value].name + " store?")) {
+        shops.splice(removeDropdown.value, 1);
+
+        for (var i = 0; i <= shops.length; i++) {
+          updateDropdown.removeChild(updateOptionList[0]);
+          removeDropdown.removeChild(removeOptionList[0]);
+        }
+
+        document.getElementById("donut-estimate").innerHTML = "";
+        renderTable();
+
+        renderDropdown();
+        removeDropdown.value = "";
+        updateDropdown.value = "";
+      } else {
+        removeDropdown.value = "";
+      }
+    }
+
+    console.log(shops);
+  }
+
+  var addButton = document.getElementById("add");
+  var updateDropdown = document.getElementById("update-dropdown");
+  var updateButton = document.getElementById("update");
+  var removeButton = document.getElementById("remove");
+
+  addButton.addEventListener("click", addNewShop, false);
+  updateDropdown.addEventListener("change", populateUpdate, false);
   updateButton.addEventListener("click", updateShop, false);
-  selectButton.addEventListener("change", populateUpdate, false);
+  removeButton.addEventListener("click", removeShop, false);
 
   generateCalc();
 
